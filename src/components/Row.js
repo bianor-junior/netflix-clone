74ec579e084674587f5dfe5c507f1988e57cee8e
Bianor Ramos <bianor.tech@gmail.com>
2022-10-13 12:00:08 -0300

    teste

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,38 +1,38 @@
-import React, { useEffect } from "react";
-import { getMovies } from "../api";
-import "./Row.css"; 
+import React, {useEffect} from "react";
+import {getMovies} from "../api";
+import "./Row.css";
 
-const imageHost = "https://image.tmdb.org/t/p/original/"
-function Row({ title, path, isLarge }) {
+const imageHost = "https://image.tmdb.org/t/p/original"
+
+function Row({title, path, isLarge}) {
     const [movies, setMovies] = React.useState([])
-    
+
     const fetchMovies = async (_path) => {
-      try{
-        const data = await getMovies(_path);
-        console.log("data", data)
-        setMovies(data?.results)
-      }catch(error) { console.log ("fetchMovies error: ", error)}
+        try {
+            const data = await getMovies(_path);
+            setMovies(data?.results)
+        } catch (error) {
+            console.log("fetchMovies error: ", error)
+        }
     }
 
     useEffect(() => {
-      fetchMovies(path);
+        fetchMovies(path);
     }, [path])
-    
-    return (
-      <div className="row-container">
-        <h2 className="row-header">{title}</h2>
-        <div className="row-cards">
-            {movies?.map((movie) =>{
-                return <img
-                 className={`movie-card ${isLarge && "movie-card-large"}`}
-                 key={movie.id} 
-                 src={`${imageHost}${
-                 isLarge ? movie.backdrop_path : movie.poster_path}`} 
-                 alt={movie.name}></img>
-            })}
-        </div>
-      </div>      
-  )
+
+    return (<div className="row-container">
+            <h2 className="row-header">{title}</h2>
+            <div className="row-cards">
+                {movies?.map((movie) => {
+                    return <img
+                        className={`movie-card ${isLarge && "movie-card-large"}`}
+                        key={movie.id}
+                        src={`${imageHost}${isLarge ? movie.backdrop_path : movie.poster_path}`}
+                        alt={movie.name}></img>
+                })}
+            </div>
+        </div>)
 }
 
 export default Row
+
